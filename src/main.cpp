8fb977079e2c683ab13dfb43a65de3ad6e737d9f
Cloud <cloudismehi@gmail.com>
2025-05-23 01:49:17 -0400

    update README and TODO list in main

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,14 @@
 
 int numVoices = 3; 
 
+//TODO: the events class is simplistic
+/*
+as of right now it only takes functions with one argument for new value and that return void. 
+i need to overload this function for situations where i am affecting one voice in particular as opposed to 
+all voices at once - keep both for both situations. the void return i won't concern myself with as far as 
+queue goes because why would i
+*/
+//TODO: review Voice / SineOcillator architecture to make sure it works with current project layout.
 
 int main(){
 	Audio audioInstance; 
